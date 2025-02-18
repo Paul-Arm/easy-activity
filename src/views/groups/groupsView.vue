bc8d60682337f2f9cc6d0a43ef2986f396206b45
Sebastian <sebastian.ostanin@icloud.com>
2025-02-18 12:29:36 +0100

    frontend activity creation features

--- src/views/groups/groupsView.vue
+++ src/views/groups/groupsView.vue
@@ -44,6 +44,20 @@
                 </div>
                 <p><strong>Gruppe ID:</strong> {{ modalGroupID }}</p>
                 <p><strong>Gruppenname:</strong> {{ modalGroupName }}</p>
+
+                <b-button v-b-toggle.collapse-group-members variant="link" @click="fetchGroupMembers(modalGroupID)">Gruppenmitglieder&nbsp;<b-icon icon="chevron-down"></b-icon></b-button>
+                <b-collapse id="collapse-group-members" class="mt-2">
+                    <b-card>
+                        <ul class="m-0">
+                            <li v-for="(groupMember, index) in modalGroupMembers" :key="index">
+                                {{ groupMember.Nachname }}, {{ groupMember.Vorname }} ({{ groupMember.Nutzername }})
+                            </li>
+                        </ul>
+                    </b-card>
+                </b-collapse>
+
+                <hr>
+                <h6>Benutzer einladen</h6>
                 <b-input-group class="mt-3">
                     <b-form-input v-model="userEmail" type="email" style="border-radius: 0.375rem 0 0 0.375rem;" placeholder="E-Mail des Nutzers"></b-form-input>
                     <b-input-group-append>
@@ -52,21 +66,119 @@
                 </b-input-group>
 
                 <hr>
-                <b-button v-b-toggle.collapse-group-members variant="secondary" @click="fetchGroupMembers(modalGroupID)">Gruppenmitglieder&nbsp;<b-icon icon="chevron-down"></b-icon></b-button>
-                <b-collapse id="collapse-group-members" class="mt-2">
-                    <b-card>
-                        <ul>
-                            <li v-for="(groupMember, index) in modalGroupMembers" :key="index">
-                                {{ groupMember.Nachname }}, {{ groupMember.Vorname }} ({{ groupMember.Nutzername }})
-                            </li>
-                        </ul>
-                    </b-card>
-                </b-collapse>
+                <div>
+                    <div>Aktivitäten in dieser Gruppe:</div>
+                    <b-button style="font-size: 85%;" v-b-toggle.collapse-new-activity variant="link">Aktivität erstellen&nbsp;<b-icon icon="chevron-down"></b-icon></b-button>
+                    <b-collapse id="collapse-new-activity" class="mt-2">
+                        <b-card>
+                            <b-form-group
+                                class="mb-0"
+                                >
+                                <b-form-group
+                                    label="Titel:"
+                                    label-for="title"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="title"></b-form-input>
+                                </b-form-group>
 
-                <div v-if="groupActivities && groupActivities.length > 0">
-                    <hr>
-                    Aktivitäten in dieser Gruppe:
-                    <ul>
+                                <b-form-group
+                                    label="Beschreibung:"
+                                    label-for="description"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="description"></b-form-input>
+                                </b-form-group>
+
+                                <hr>
+
+                                <b-form-group
+                                    label="Straße:"
+                                    label-for="street"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="street"></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Hausnummer:"
+                                    label-for="number"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="number"></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Postleitzahl:"
+                                    label-for="postal-code"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="postal-code"></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Ort:"
+                                    label-for="city"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="city"></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Staat:"
+                                    label-for="state"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-input id="state"></b-form-input>
+                                </b-form-group>
+
+                                <hr>
+
+                                <b-form-group
+                                    label="Startzeitpunkt:"
+                                    label-for="starttime"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-datepicker id="example-datepicker" class="mb-2"></b-form-datepicker>
+                                    <b-form-timepicker locale="de"></b-form-timepicker>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Endzeitpunkt: (optional)"
+                                    label-for="endtime"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-datepicker id="example-datepicker" class="mb-2"></b-form-datepicker>
+                                    <b-form-timepicker locale="de"></b-form-timepicker>
+                                </b-form-group>
+
+                                <b-form-group
+                                    label="Abstimmungsende:"
+                                    label-for="voting-deadline"
+                                    label-cols-sm="3"
+                                    label-align-sm="right"
+                                >
+                                    <b-form-datepicker id="example-datepicker" class="mb-2"></b-form-datepicker>
+                                    <b-form-timepicker locale="de"></b-form-timepicker>
+                                </b-form-group>
+                                
+                                <hr>
+
+                                <b-button variant="success" class="mt-2">Aktivität erstellen</b-button>
+                            </b-form-group>
+                        </b-card>
+                    </b-collapse>
+
+                    <ul v-if="groupActivities && groupActivities.length > 0">
                         <li v-for="(groupActivity, index) in groupActivities" :key="index">
                             {{ groupActivity.Titel }}
                         </li>
@@ -124,9 +236,9 @@ import axios from "axios";
                 });
 
             axios
-                .get(this.apiUrl + "/Aktivitaet")
+                .get(this.apiUrl + "/Aktivitaet/MeineAktivitaeten")
                 .then((response) => {
-                    this.groupActivities = response.data.content.filter(activity => activity.Gruppe === groupID);
+                    this.groupActivities = response.data.filter(activity => activity.Gruppe === groupID);
                 })
                 .catch((error) => {
                     console.error("Error fetching data:", error);
